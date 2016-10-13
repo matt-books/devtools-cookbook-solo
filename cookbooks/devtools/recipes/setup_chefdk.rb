c14a9b776c438715e3aa2c6e3104ee95f76076f4
User account for Matt Feenstra <matt_feenstra@ip-10-252-1-35.ycudev.internal>
2016-10-13 01:21:38 +0000

    updates for kitchen-ec2

--- cookbooks/devtools/recipes/setup_chefdk.rb
+++ cookbooks/devtools/recipes/setup_chefdk.rb
@@ -17,13 +17,21 @@ execute 'gem_install_testkitchen' do
 end
 
 ### install kitchen-vagrant gem
-execute 'gem_install_kitchenvagrant' do
-        command "su #{username} -l -c 'source ~#{username}/.rvm/scripts/rvm && gem install kitchen-vagrant'"
-end
+#execute 'gem_install_kitchenvagrant' do
+#        command "su #{username} -l -c 'source ~#{username}/.rvm/scripts/rvm && gem install kitchen-vagrant'"
+#end
 
 ### install kitchen-inspec gem
-execute 'gem_install_kitcheninspec' do
-        command "su #{username} -l -c 'source ~#{username}/.rvm/scripts/rvm && gem install kitchen-inspec'"
+#execute 'gem_install_kitcheninspec' do
+#        command "su #{username} -l -c 'source ~#{username}/.rvm/scripts/rvm && gem install kitchen-inspec'"
+#end
+
+### install kitchen AWS gem
+execute 'gem_install_kitchen_ec2' do
+        command "su #{username} -l -c 'source ~#{username}/.rvm/scripts/rvm && gem install kitchen-ec2'"
 end
 
 
+execut 'gem_install_chefdk' do
+	command "su #{username} -l -c 'source ~#{username}/.rvm/scripts/rvm && gem install chef-dk'"
+end
